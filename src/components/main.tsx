b903a52065e446d0a2bc0f947496cf8ff4016896
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-10-23 23:30:16 +0300

    добавил компоненту сцены

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import Scene from './scene';
 
 const MainContainer = styled.main`
   width: 100%;
+  margin: 0 auto;
   padding: 0 80px;
-  position: relative;
-  background-color: white;
-`;
+  `;
 
 const Title = styled.h1`
   max-width: 353px;
   width: 100%;
-  left: 0;
-  top: 0;
-  position: absolute;
+  position: relative;
+  top: 170px;
+  font-weight: bold;
+  font-size: 56px;
 `;
 
 const Main = (): React.JSX.Element => {
   return (
     <MainContainer>
       <Title>Исторические даты</Title>
+      <Scene />
     </MainContainer>
   );
 };
