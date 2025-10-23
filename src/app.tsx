2afd6577b98bf065c59adb1e7aeecde32f2f37bf
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-10-23 20:20:36 +0300

    добавил контейнер-оболочку приложения

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import Main from './components/main';
 
+const Window = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: green;
+`;
+
+const Container = styled.div`
+  max-width: 1440px;
+  widht: 100%;
+`;
 const App = (): React.JSX.Element => {
-  return <div>Hello, App!</div>;
+  return (
+    <Window>
+      <Container>
+        <Main />
+      </Container>
+    </Window>
+  );
 };
 export default App;
