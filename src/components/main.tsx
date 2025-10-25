cc7a3ec2e43e2eb3c6a02a3c7b7e57fd769cf618
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-10-25 23:23:02 +0300

    добавил компоненту слайдера

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import Scene from './scene';
+import Slider from './slider';
 
 const MainContainer = styled.main`
   width: 100%;
   margin: 0 auto;
   padding: 0 80px;
-  `;
+  padding-bottom: 20px;
+`;
 
 const Title = styled.h1`
   max-width: 353px;
@@ -15,6 +17,7 @@ const Title = styled.h1`
   top: 170px;
   font-weight: bold;
   font-size: 56px;
+  color: #42567a;
 `;
 
 const Main = (): React.JSX.Element => {
@@ -22,6 +25,7 @@ const Main = (): React.JSX.Element => {
     <MainContainer>
       <Title>Исторические даты</Title>
       <Scene />
+      <Slider />
     </MainContainer>
   );
 };
